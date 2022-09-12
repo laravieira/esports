1cd7ddbe1f2ac3d12174773660467ebb2c55c2b7
Lara Vieira <contato@laravieira.me>
2022-09-12 20:40:01 -0300

    Made static landing web page following class 2.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,73 @@
+import './styles/main.css';
+import logo from './assets/logo.svg';
+import { MagnifyingGlassPlus } from 'phosphor-react';
+
 function App() {
-  return <h1>Hello NLW</h1>;
+  return (
+      <div className="max-w-[1344px] flex flex-col items-center m-20">
+        <img src={ logo }  alt="eSports logo from NLW 9"/>
+        <h1 className="text-6xl text-white font-black mt-20">Seu <span className="bg-nlw-gradient text-transparent bg-clip-text">duo</span> está aqui.</h1>
+
+        <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="#" className="relative rounded-lg overflow-hidden">
+            <img src="/game-1.png"  alt=""/>
+            <div className="m-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">League of Legends</strong>
+                <span className="text-zinc-300 text-sm block">X anúncios</span>
+            </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+            <img src="/game-2.png" alt=""/>
+            <div className="m-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">Dota 2</strong>
+                <span className="text-zinc-300 text-sm block">X anúncios</span>
+            </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+            <img src="/game-3.png" alt=""/>
+            <div className="m-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">Counter Strike</strong>
+                <span className="text-zinc-300 text-sm block">X anúncios</span>
+            </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+            <img src="/game-4.png" alt=""/>
+            <div className="m-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">Apex Legends</strong>
+                <span className="text-zinc-300 text-sm block">X anúncios</span>
+            </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+            <img src="/game-5.png" alt=""/>
+            <div className="m-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">Fortnite</strong>
+                <span className="text-zinc-300 text-sm block">X anúncios</span>
+            </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+            <img src="/game-6.png" alt=""/>
+            <div className="m-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">World of Warcraft</strong>
+                <span className="text-zinc-300 text-sm block">X anúncios</span>
+            </div>
+        </a>
+        </div>
+
+        <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden"></div>
+        <div className="bg-[#2A2634] px-8 py-6 flex self-stretch justify-between items-center">
+            <div>
+                <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+                <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+            </div>
+            <div>
+                <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3">
+                    <MagnifyingGlassPlus size="24"></MagnifyingGlassPlus>
+                    Publicar anúncio
+                </button>
+            </div>
+        </div>
+      </div>
+  );
 }
 
 export default App;
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './src/**/*.tsx',
+      './index.html'
+  ],
+  theme: {
+    fontFamily: {
+      sans: [
+        'Inter',
+        'sans-serif'
+      ]
+    },
+    extend: {
+      backgroundImage: {
+        background: 'url(/background.png)',
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 0%, #43E7AD 50%, #E1D55D 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);',
+      }
+    },
+  },
+  plugins: [],
+}
